137612a5f8391e9cdfdc6f08bb302d203710a7e1
GouZ <sylvain.gougouzian@gmail.com>
2015-03-16 22:04:05 +0100

    [*] repartition ok

--- dist/flub.js
+++ dist/flub.js
@@ -3,32 +3,49 @@
 
   Flub = (function() {
     function Flub(selector, options) {
+      var flubber;
       this.options = options != null ? options : {};
-      this.element = document.querySelector(selector);
+      flubber = document.querySelector(selector);
+      this.element = flubber.querySelector(flubber.getAttribute('data-launcher'));
       this.init();
+      this.makeFilter();
+      flubber.style.filter = 'url(#flub-shadow)';
+      flubber.style.webkitFilter = 'url(#flub-shadow)';
+      this.bind();
       this;
     }
 
+    Flub.prototype.makeFilter = function() {
+      var svg;
+      svg = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1"> <defs> <filter id="flub-shadow"> <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="' + this.button.radius / 2 + '" /> <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" /> <feGaussianBlur in="goo" stdDeviation="3" result="shadow" /> <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 0 0" result="shadow" /> <feOffset in="shadow" dx="1" dy="1" result="shadow" /> <feBlend in2="shadow" in="goo" result="goo" /> <feBlend in2="goo" in="SourceGraphic" result="mix" /> </filter> <filter id="flub-goo"> <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="5" /> <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" /> <feBlend in2="goo" in="SourceGraphic" result="mix" /> </filter> </defs> </svg>';
+      return document.write(svg);
+    };
+
     Flub.prototype.init = function() {
-      var base, base1, button, cnst, i, items, j, k, len, len1, radius, rect, ref, wrapper;
+      var radius, rect;
+      this.wrapper = document.querySelector(this.element.getAttribute('href'));
+      rect = this.element.getBoundingClientRect();
+      radius = (rect.right - rect.left) / 2;
+      return this.button = {
+        top: rect.top + document.body.scrollTop + radius,
+        left: rect.left + document.body.scrollLeft + radius,
+        radius: radius
+      };
+    };
+
+    Flub.prototype.bind = function() {
+      var base, base1, cnst, i, items, j, k, len, len1, radius, rect, ref;
       if ((base = this.options).angle_max == null) {
         base.angle_max = 360;
       }
       if ((base1 = this.options).angle_min == null) {
         base1.angle_min = 0;
       }
-      wrapper = document.querySelector(this.element.getAttribute('href'));
-      rect = this.element.getBoundingClientRect();
-      radius = (rect.right - rect.left) / 2;
-      button = {
-        top: rect.top + document.body.scrollTop + radius,
-        left: rect.left + document.body.scrollLeft + radius,
-        radius: radius
-      };
+      this.options.speed = 200;
       items = [];
       this.element.style.position = 'relative';
       this.element.style.zIndex = 2;
-      ref = wrapper.children;
+      ref = this.wrapper.children;
       for (j = 0, len = ref.length; j < len; j++) {
         i = ref[j];
         if (i.nodeType !== 8) {
@@ -40,16 +57,18 @@
       radius = (rect.right - rect.left) / 2;
       for (k = 0, len1 = items.length; k < len1; k++) {
         i = items[k];
-        i.style.left = (button.left - radius) + 'px';
-        i.style.top = (button.top - radius) + 'px';
+        i.style.left = (this.button.left - radius) + 'px';
+        i.style.top = (this.button.top - radius) + 'px';
+        i.style.transition = 'all ease-out ' + this.options.speed + 'ms';
       }
       cnst = Math.PI / 180;
+      this.element;
       return this.element.addEventListener('click', (function(_this) {
         return function(e) {
-          var D, N, a, c, calc, f, l, len2, len3, m, n, o, results, results1;
+          var D, N, a, ang, c, calc, f, l, len2, len3, m, n, o, results, results1;
           e.preventDefault();
-          if (wrapper.classList.contains('open')) {
-            wrapper.classList.remove('open');
+          if (_this.wrapper.classList.contains('open')) {
+            _this.wrapper.classList.remove('open');
             results = [];
             for (n = 0, len2 = items.length; n < len2; n++) {
               i = items[n];
@@ -58,16 +77,17 @@
             }
             return results;
           } else {
-            wrapper.classList.add('open');
+            _this.wrapper.classList.add('open');
             l = items.length;
-            D = 2 * button.radius;
+            D = 2 * _this.button.radius;
             N = f = 0;
+            ang = _this.options.angle_max - _this.options.angle_min;
             calc = function() {
-              N = ~~(6 * ((_this.options.angle_max - _this.options.angle_min) / 360) * (1 + D / button.radius));
+              N = ~~((ang / 60) * (D / (radius * 2)));
               if (N > l) {
                 N = l;
               }
-              return f = (_this.options.angle_max - _this.options.angle_min) / N;
+              return f = ang / N;
             };
             calc();
             c = 0;
@@ -78,7 +98,7 @@
               if (c === N) {
                 c = 0;
                 m++;
-                D += 2 * button.radius;
+                D += 2 * _this.button.radius;
                 l -= N;
                 calc();
               }
--- src/flub.coffee
+++ src/flub.coffee
@@ -1,48 +1,80 @@
 class Flub
   constructor: (selector, @options = {}) ->
-    @element = document.querySelector selector
+    flubber = document.querySelector selector
+    @element = flubber.querySelector flubber.getAttribute 'data-launcher'
     @init()
+    @makeFilter()
+    flubber.style.filter = 'url(#flub-shadow)'
+    flubber.style.webkitFilter = 'url(#flub-shadow)'
+    @bind()
     @
+  makeFilter: ->
+    svg = '
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+      <defs>
+        <filter id="flub-shadow">
+            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="' + @button.radius / 2 + '" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+            <feGaussianBlur in="goo" stdDeviation="3" result="shadow" />
+            <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 0 0" result="shadow" />
+            <feOffset in="shadow" dx="1" dy="1" result="shadow" />
+            <feBlend in2="shadow" in="goo" result="goo" />
+            <feBlend in2="goo" in="SourceGraphic" result="mix" />
+        </filter>
+        <filter id="flub-goo">
+            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="5" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+            <feBlend in2="goo" in="SourceGraphic" result="mix" />
+        </filter>
+      </defs>
+    </svg>
+    '
+    document.write svg
   init: ->
-    @options.angle_max ?= 360
-    @options.angle_min ?= 0
-    wrapper = document.querySelector @element.getAttribute 'href'
+    @wrapper = document.querySelector @element.getAttribute 'href'
     rect = @element.getBoundingClientRect()
     radius = (rect.right - rect.left) / 2
-    button = {
+    @button = {
       top: rect.top + document.body.scrollTop + radius
       left: rect.left + document.body.scrollLeft + radius
       radius: radius
     }
+  bind: ->
+    @options.angle_max ?= 360
+    @options.angle_min ?= 0
+    @options.speed = 200
     items = []
     @element.style.position = 'relative'
     @element.style.zIndex = 2
-    for i in wrapper.children
+    for i in @wrapper.children
       if i.nodeType isnt 8
         i.style.position = 'absolute'
         items.push i
     rect = items[0].getBoundingClientRect()
     radius = (rect.right - rect.left) / 2
     for i in items
-      i.style.left = (button.left - radius) + 'px'
-      i.style.top = (button.top - radius) + 'px'
+      i.style.left = (@button.left - radius) + 'px'
+      i.style.top = (@button.top - radius) + 'px'
+      i.style.transition = 'all ease-out ' + @options.speed + 'ms'
     cnst = (Math.PI / 180)
+    @element
     @element.addEventListener 'click', (e) =>
       e.preventDefault()
-      if wrapper.classList.contains 'open'
-        wrapper.classList.remove 'open'
+      if @wrapper.classList.contains 'open'
+        @wrapper.classList.remove 'open'
         for i in items
           i.style.marginLeft = 0
           i.style.marginTop = 0
       else
-        wrapper.classList.add 'open'
+        @wrapper.classList.add 'open'
         l = items.length
-        D = 2 * button.radius
+        D = 2 * @button.radius
         N = f = 0
+        ang = @options.angle_max-@options.angle_min
         calc = () =>
-          N = ~~(6 * ((@options.angle_max-@options.angle_min)/360) * (1 + D / button.radius))
+          N = ~~((ang / 60) * (D / (radius * 2)))
           N = l if N > l
-          f = (@options.angle_max - @options.angle_min) / N
+          f = ang / N
         calc()
         c = 0
         m = 0
@@ -50,7 +82,7 @@ class Flub
           if c is N
             c = 0
             m++
-            D += 2 * button.radius
+            D += 2 * @button.radius
             l -= N
             calc()
           a = cnst * (@options.angle_min + f * c++)
