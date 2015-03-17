f951a31956c0152fe0771d4f54bc6b65f8d9b81d
GouZ <sylvain.gougouzian@gmail.com>
2015-03-17 21:52:28 +0100

    [+] example

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+var flub = new Flub('#sandbox-wheel', {
+  min: 0,
+  max: 360,
+  speed: 800,
+  dispatch: true,
+  sync: false
+});
+var a = document.querySelector('#add'),
+  ul = document.querySelector('#menu');
+a.addEventListener('click', function(e) {
+  var li = document.createElement('li');
+  li.innerHTML = '<a href="#">O</a>';
+  ul.appendChild(li);
+  flub.populate();
+  return false;
+}, false);
